b95624faaa8896bb781e3a59e3744de2817c117e
pdpan0 <lm.m4rtins@gmail.com>
2024-03-23 11:17:09 -0300

    refact: alterando nome

--- ms-users/src/main/kotlin/br/com/pdpano/msusers/MsUsersApplication.kt
+++ ms-users/src/main/kotlin/br/com/pdpano/msusers/MsUsersApplication.kt
@@ -4,8 +4,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
-class MsTransactionsApplication
+class MsUsersApplication
 
 fun main(args: Array<String>) {
-	runApplication<MsTransactionsApplication>(*args)
+	runApplication<MsUsersApplication>(*args)
 }
