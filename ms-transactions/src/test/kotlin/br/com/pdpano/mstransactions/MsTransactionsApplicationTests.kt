c8a6de65eff3bc9690f530ede373cdfd4014a0b9
pdpan0 <lm.m4rtins@gmail.com>
2024-03-22 22:50:11 -0300

    feat: initial commit

---
+++ ms-transactions/src/test/kotlin/br/com/pdpano/mstransactions/MsTransactionsApplicationTests.kt
@@ -0,0 +1,13 @@
+package br.com.pdpano.mstransactions
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MsTransactionsApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
---
+++ ms-users/src/main/kotlin/br/com/pdpano/mstransactions/MsTransactionsApplication.kt
@@ -0,0 +1,11 @@
+package br.com.pdpano.mstransactions
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MsTransactionsApplication
+
+fun main(args: Array<String>) {
+	runApplication<MsTransactionsApplication>(*args)
+}
