fecb48c4a6de6954facd085625b592060158f9e6
pdpan0 <lm.m4rtins@gmail.com>
2024-03-24 13:32:30 -0300

    fix: necessário salvar o código do enum e não a ordem

--- ms-transactions/src/main/kotlin/br/com/pdpano/mstransactions/infra/database/entities/TransactionEntity.kt
+++ ms-transactions/src/main/kotlin/br/com/pdpano/mstransactions/infra/database/entities/TransactionEntity.kt
@@ -1,8 +1,10 @@
 package br.com.pdpano.mstransactions.infra.database.entities
 
-import br.com.pdpano.mstransactions.domain.TransactionStatus
+import br.com.pdpano.mstransactions.domain.transactions.TransactionStatus
 import br.com.pdpano.mstransactions.domain._dto.CreateTransactionDTO
 import jakarta.persistence.Entity
+import jakarta.persistence.EnumType
+import jakarta.persistence.Enumerated
 import jakarta.persistence.GeneratedValue
 import jakarta.persistence.GenerationType
 import jakarta.persistence.Id
@@ -15,6 +17,7 @@ data class TransactionEntity(
     val idTransaction: Long?,
     val idPayer: Long,
     val idPayee: Long,
+    @Enumerated(EnumType.STRING)
     val status: TransactionStatus = TransactionStatus.PENDING,
     val vlTransaction: BigDecimal,
     val createdAt: LocalDateTime = LocalDateTime.now()
