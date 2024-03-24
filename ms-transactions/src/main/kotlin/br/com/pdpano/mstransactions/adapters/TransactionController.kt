d342f0c52c92a82eafe3ffc47cea8a60cffe3b7d
pdpan0 <lm.m4rtins@gmail.com>
2024-03-24 13:32:50 -0300

    feat: implementação do endpoint de criação

--- ms-transactions/src/main/kotlin/br/com/pdpano/mstransactions/adapters/TransactionController.kt
+++ ms-transactions/src/main/kotlin/br/com/pdpano/mstransactions/adapters/TransactionController.kt
@@ -1,19 +1,29 @@
 package br.com.pdpano.mstransactions.adapters
 
 import br.com.pdpano.mstransactions.domain._dto.CreateTransactionDTO
-import org.springframework.web.bind.annotation.GetMapping
+import br.com.pdpano.mstransactions.usecase.createtransaction.CreateTransactionUseCase
+import br.com.pdpano.mstransactions.domain._response.ResponseMessage
+import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
+import java.net.URI
 
 @RestController
 @RequestMapping("v1/transactions")
-class TransactionController(): TransactionControllerSwagger {
+class TransactionController(
+    private val createTransactionUseCase: CreateTransactionUseCase
+): TransactionControllerSwagger {
 
     @PostMapping
-    override fun createTransaction(@RequestBody transaction: CreateTransactionDTO) {
-        TODO("Not yet implemented")
-    }
+    override fun createTransaction(
+        @RequestBody transaction: CreateTransactionDTO
+    ): ResponseEntity<ResponseMessage<Long>> =
+        createTransactionUseCase.execute(transaction).let { idTransaction ->
+            ResponseEntity
+                .created(URI("/v1/transactions/$idTransaction"))
+                .body(ResponseMessage.build(idTransaction))
+        }
 
 }
