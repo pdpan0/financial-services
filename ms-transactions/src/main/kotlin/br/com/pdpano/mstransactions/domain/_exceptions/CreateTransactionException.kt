d1c3d72f10b8c08cf85c467f04c4316ac4a28bc2
pdpan0 <lm.m4rtins@gmail.com>
2024-03-26 19:25:41 -0300

    refact: removendo construtor não utilizado

--- ms-transactions/src/main/kotlin/br/com/pdpano/mstransactions/domain/_exceptions/CreateTransactionException.kt
+++ ms-transactions/src/main/kotlin/br/com/pdpano/mstransactions/domain/_exceptions/CreateTransactionException.kt
@@ -3,6 +3,4 @@ package br.com.pdpano.mstransactions.domain._exceptions
 class CreateTransactionException(
     override val message: String? = "Unable to register transaction",
     override val cause: Throwable? = null
-): RuntimeException() {
-    constructor(cause: Throwable?): this(null, cause)
-}
+): RuntimeException()
