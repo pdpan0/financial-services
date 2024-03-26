2dd43fe77cdc6206344088244cb6ab4063fb0e0e
pdpan0 <lm.m4rtins@gmail.com>
2024-03-26 19:27:07 -0300

    refact: alterando validação para criação do usuário, pois validação não atende a outro escopo

--- ms-users/src/main/kotlin/br/com/pdpano/msusers/usecase/createuser/CreateUserUseCaseImpl.kt
+++ ms-users/src/main/kotlin/br/com/pdpano/msusers/usecase/createuser/CreateUserUseCaseImpl.kt
@@ -2,17 +2,26 @@ package br.com.pdpano.msusers.usecase.createuser
 
 import br.com.pdpano.msusers.domain.UserGateway
 import br.com.pdpano.msusers.domain._dto.CreateUserDTO
-import br.com.pdpano.msusers.usecase.validateuser.ValidateUserUseCase
+import br.com.pdpano.msusers.domain._exceptions.CreateUserException
+import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Service
+import java.math.BigDecimal
 
 @Service
-class CreateUserUseCaseImpl(
-    private val validateUserUseCase: ValidateUserUseCase,
-    private val gateway: UserGateway
-): CreateUserUseCase {
+class CreateUserUseCaseImpl(private val gateway: UserGateway): CreateUserUseCase {
+    private val logger = LoggerFactory.getLogger(this::class.java)
 
     override fun execute(input: CreateUserDTO): Long {
-        validateUserUseCase.execute(input)
+        if (gateway.existUser(input.email, input.taxNumber)) {
+            logger.debug("User already exists {}", input)
+            throw CreateUserException("User already exists")
+        }
+
+        if (input.vlBalance < BigDecimal.ZERO) {
+            logger.debug("User cannot be created with negative balance {}", input)
+            throw CreateUserException("User cannot be created with negative balance")
+        }
+
         return gateway.createUser(input)
     }
 }
--- ms-users/src/main/kotlin/br/com/pdpano/msusers/usecase/validateuser/ValidateUserUseCase.kt
+++
@@ -1,8 +0,0 @@
-package br.com.pdpano.msusers.usecase.validateuser
-
-import br.com.pdpano.msusers.domain._dto.CreateUserDTO
-import br.com.pdpano.msusers.usecase.UseCase
-
-interface ValidateUserUseCase: UseCase<CreateUserDTO, Boolean> {
-    override fun execute(input: CreateUserDTO): Boolean
-}
