7d113e340e4995bb455d2f1fadb85378581f1c85
pdpan0 <lm.m4rtins@gmail.com>
2024-03-26 19:39:47 -0300

    feat: adicionando open feign

--- ms-users/src/main/kotlin/br/com/pdpano/msusers/MsUsersApplication.kt
+++ ms-users/src/main/kotlin/br/com/pdpano/msusers/MsUsersApplication.kt
@@ -2,7 +2,9 @@ package br.com.pdpano.msusers
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.cloud.openfeign.EnableFeignClients
 
+@EnableFeignClients
 @SpringBootApplication
 class MsUsersApplication
 
