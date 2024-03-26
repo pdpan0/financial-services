0b990105fb7013c0fd191fd750426901c90f8e10
pdpan0 <lm.m4rtins@gmail.com>
2024-03-26 19:26:15 -0300

    feat: tratando cenários inválidos dos clientes

--- ms-transactions/src/main/kotlin/br/com/pdpano/mstransactions/usecase/createtransaction/CreateTransactionUseCaseImpl.kt
+++ ms-transactions/src/main/kotlin/br/com/pdpano/mstransactions/usecase/createtransaction/CreateTransactionUseCaseImpl.kt
@@ -1,19 +1,25 @@
 package br.com.pdpano.mstransactions.usecase.createtransaction
 
+import br.com.pdpano.mstransactions.domain.AuthenticatorGateway
+import br.com.pdpano.mstransactions.domain.NotificationGateway
+import br.com.pdpano.mstransactions.domain.UserGateway
 import br.com.pdpano.mstransactions.domain.transactions.TransactionGateway
 import br.com.pdpano.mstransactions.domain._dto.CreateTransactionDTO
+import br.com.pdpano.mstransactions.domain._dto.TransferBalanceDTO
 import br.com.pdpano.mstransactions.domain._exceptions.CreateTransactionException
-import br.com.pdpano.mstransactions.infra.clients.AuthenticatorClient
-import br.com.pdpano.mstransactions.infra.clients.NotificationClient
-import br.com.pdpano.mstransactions.infra.clients.UserClient
+import br.com.pdpano.mstransactions.domain._exceptions.RequestClientException
+import feign.FeignException
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Service
 
 @Service
 class CreateTransactionUseCaseImpl(
-    private val client: AuthenticatorClient,
-    private val notificationClient: NotificationClient,
-    private val userClient: UserClient,
+    private val client: AuthenticatorGateway,
+    private val notificationClient: NotificationGateway,
+    private val userClient: UserGateway,
     private val transactionGateway: TransactionGateway
 ): CreateTransactionUseCase {
     private val logger = LoggerFactory.getLogger(this::class.java)
@@ -40,17 +46,42 @@ class CreateTransactionUseCaseImpl(
             throw CreateTransactionException("The payer does not have sufficient balance")
         }
 
-        if (client.authenticate().message != "Autorizado") {
+        if (requestAuthenticateClient()) {
             throw CreateTransactionException("Transaction not allowed")
         }
 
-        val idTransaction = transactionGateway.createTransaction(input)
+         val idTransaction = transactionGateway.createTransaction(input)
 
-        if (notificationClient.notify().message) {
-            logger.info("Message sent with successfully")
+        userClient.transferBalance(TransferBalanceDTO(idTransaction, payee.idUser, payer.idUser, input.vlTransaction))
+
+        CoroutineScope(Dispatchers.Default).launch {
+            notify()
         }
 
         return idTransaction
     }
 
+
+    private fun requestAuthenticateClient(): Boolean = try {
+        client.authenticate().message == "Autorizado"
+    } catch (e: FeignException) {
+        logger.error("OpenFeign returns an error: {}", e.message, e)
+        throw RequestClientException("authenticator", e)
+    }
+
+    private suspend fun notify() {
+        try {
+            if (requestNotificationClient()) {
+                logger.info("Message sent with successfully")
+            }
+        } catch (e: RequestClientException) {
+            logger.info("Message was not send with successfully")
+        }
+    }
+    private fun requestNotificationClient(): Boolean = try {
+        notificationClient.notify().message
+    } catch (e: FeignException) {
+        logger.error("OpenFeign returns an error: {}", e.message, e)
+        throw RequestClientException("notifier", e)
+    }
 }
