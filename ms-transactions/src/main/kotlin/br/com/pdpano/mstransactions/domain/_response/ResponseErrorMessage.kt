d6b92cf10d7013a038eca0b1a4622d251e7e6a8d
pdpan0 <lm.m4rtins@gmail.com>
2024-03-26 19:44:20 -0300

    refact: novo método para retorno das mensagens de erro passando parametro objects

--- ms-transactions/src/main/kotlin/br/com/pdpano/mstransactions/domain/_response/ResponseErrorMessage.kt
+++ ms-transactions/src/main/kotlin/br/com/pdpano/mstransactions/domain/_response/ResponseErrorMessage.kt
@@ -7,6 +7,10 @@ data class ResponseErrorMessage<T>(
     val objects: T?
 ) {
     companion object {
+        fun <T> build(throwable: Throwable, objects: T): ResponseErrorMessage<T> {
+            return ResponseErrorMessage(false, throwable.javaClass.getSimpleName(), throwable.message, objects)
+        }
+
         fun build(throwable: Throwable): ResponseErrorMessage<Nothing> {
             return ResponseErrorMessage(false, throwable.javaClass.getSimpleName(), throwable.message, null)
         }
